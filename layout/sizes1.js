4d62f89a8f7505979b1dc1a8eda73ba9546d348c
Ushakov Alexandr <renuxelastudy@gmail.com>
2024-10-31 00:06:45 +0300

    modified:   layout/fonst.css 	modified:   layout/index1.html 	modified:   layout/sizes1.js 	modified:   layout/style1.css

--- layout/sizes1.js
+++ layout/sizes1.js
@@ -8,4 +8,21 @@ window.onload = function() {
     const secondWidth = parseFloat(getComputedStyle(secondElem).width);
     const finishMargin = (firstLeftMargin - secondWidth) / 2;
     secondElem.style["margin-left"] = `${finishMargin}px`;
+
+    const h1 = document.getElementsByTagName("h1");
+    const elemh1 = h1[0];
+    elemh1.style["margin-left"] = secondElem.style["margin-left"];
+
+    const h2 = document.getElementsByTagName("h2");
+    Array.from(h2).forEach(element => {
+        element.style["margin-left"] = secondElem.style["margin-left"]
+        }
+    );
+
+    const h1Size = parseFloat(getComputedStyle(h2[0])["font-size"]);
+    h2[0].style["font-size"] = `${h1Size * 18 / 25}px`;
+
+    const p = document.getElementsByTagName("p");
+    p[0].style["margin-left"] = secondElem.style["margin-left"];
+    p[0].style["font-size"] = `${h1Size * 3 / 5}px`;
 };
