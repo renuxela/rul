ee60150d2b125a14f8e3cfb31b91f7b650fbfe66
Ushakov Alexandr <renuxelastudy@gmail.com>
2024-11-30 13:08:24 +0300

    modified:   layout/morphology.html 	modified:   layout/morphology.js 	new file:   layout/nounTask1.html 	new file:   layout/nounTasks.html 	new file:   layout/style4.css 	new file:   layout/style5.css 	new file:   package-lock.json 	new file:   package.json

--- layout/morphology.js
+++ layout/morphology.js
@@ -1,45 +1,45 @@
 const independentPartsContent = `<div class="description">
 
-                    <div class="description__name">Самостоятельные<br>части речи</div>
-                    <div class="description__text">
-                        Имеют грамматическое значение:<br>
-                        обозначают предмет, признак предмета,<br>
-                        действие предмета и т.п.<br>
-                        К ним возможно поставить вопрос.<br>
-                        Являются членами предложения.<br>
-                        <br>
-                        Определяются тремя критериями:<br>
-                        1) обобщенное значение,<br>
-                        2) морфологические признаки,<br>
-                        3) синтаксическое поведение<br>
-                        (синтаксические функции и<br> 
-                        синтаксические связи).
-
-                    </div>
+                <div class="description__name">Самостоятельные<br>части речи</div>
+                <div class="description__text">
+                    Имеют грамматическое значение:<br>
+                    обозначают предмет, признак предмета,<br>
+                    действие предмета и т.п.<br>
+                    К ним возможно поставить вопрос.<br>
+                    Являются членами предложения.<br>
+                    <br>
+                    Определяются тремя критериями:<br>
+                    1) обобщенное значение,<br>
+                    2) морфологические признаки,<br>
+                    3) синтаксическое поведение<br>
+                    (синтаксические функции и<br> 
+                    синтаксические связи).
 
                 </div>
 
-                <div class="independentPartsContainer">
-                    <div class="independentParts">
+            </div>
 
-                        <div class="independentPart"><div>Существительное</div></div>
-                        <div class="line"></div>
-                        <div class="independentPart"><div>Прилагательное</div></div>
-                        <div class="line"></div>
-                        <div class="independentPart"><div>Глагол</div></div>
-                        <div class="line"></div>
-                        <div class="independentPart"><div>Местоимение</div></div>
-                        <div class="line"></div>
-                        <div class="independentPart"><div>Числительное</div></div>
-                        <div class="line"></div>
-                        <div class="independentPart"><div>Наречие</div></div>
+            <div class="independentPartsContainer">
+                <div class="independentParts">
 
-                    </div>
+                    <div class="independentPart"><div id="noun">Существительное</div></div>
+                    <div class="line"></div>
+                    <div class="independentPart"><div>Прилагательное</div></div>
+                    <div class="line"></div>
+                    <div class="independentPart"><div>Глагол</div></div>
+                    <div class="line"></div>
+                    <div class="independentPart"><div>Местоимение</div></div>
+                    <div class="line"></div>
+                    <div class="independentPart"><div>Числительное</div></div>
+                    <div class="line"></div>
+                    <div class="independentPart"><div>Наречие</div></div>
+
                 </div>
+            </div>
 
-                <div class="rewind1">
-                    <div class="scrollRight"></div>
-                </div>`;
+            <div class="rewind1">
+                <div class="scrollRight"></div>
+            </div>`;
 
 const auxiliaryParts = `<div class="rewind2">
                     <div class="scrollLeft"></div>
@@ -116,28 +116,44 @@ const interjection = `<div class="rewind4">
 
 var main = document.getElementsByTagName('main')[0];
 
+function addNounClickHandler() {
+    const noun = document.getElementById("noun");
+    if (noun) { // Проверяем, существует ли элемент
+        noun.onclick = function () {
+            window.location.href = "nounTasks.html";
+        };
+    }
+}
+
 document.addEventListener('click', function (event) {
     // Проверяем, является ли кликнутый элемент rewind1
     if (event.target.closest('.rewind1')) {
         main.innerHTML = auxiliaryParts;
         main.classList.toggle('newMain1');
+        addNounClickHandler(); // Назначаем обработчик после обновления содержимого
     }
     // Проверяем, является ли кликнутый элемент rewind2
     if (event.target.closest('.rewind2')) {
         main.innerHTML = independentPartsContent;
         main.classList.remove('newMain1');
+        addNounClickHandler(); // Назначаем обработчик после обновления содержимого
     }
     // Проверяем, является ли кликнутый элемент rewind3
     if (event.target.closest('.rewind3')) {
         main.innerHTML = interjection;
         main.classList.remove('newMain1');
         main.classList.toggle('newMain2');
+        addNounClickHandler(); // Назначаем обработчик после обновления содержимого
     }
     // Проверяем, является ли кликнутый элемент rewind4
     if (event.target.closest('.rewind4')) {
         main.innerHTML = auxiliaryParts;
         main.classList.remove('newMain2');
         main.classList.toggle('newMain1');
+        addNounClickHandler(); // Назначаем обработчик после обновления содержимого
     }
 });
+
+// Назначаем обработчик на начальном содержимом
+addNounClickHandler();
 
